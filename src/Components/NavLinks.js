0f11cd6eb4a2c993798acaf771b44c0d19a98734
LandingHeart <land.liu0123@gmail.com>
2019-02-19 16:01:21 -0500

    missing files

---
+++ src/Components/NavLinks.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {NavLink} from 'react-router-dom';
+
+const NavLinks = () =>{
+    return (
+        <ul className = 'right'>
+        <li><NavLink to = '/'> Project </NavLink></li>
+        <li><NavLink to = '/'> log out</NavLink></li>
+        <li><NavLink to = '/' className = 'btn btn-floating blue lighten-1'> BLOG </NavLink></li>
+        </ul>
+   
+    )
+}
+export default NavLinks;
