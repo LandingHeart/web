33675145893d520f6c6da7029e6ee489e3276349
LandingHeart <land.liu0123@gmail.com>
2019-02-20 09:07:25 -0500

    updates

--- src/Components/Logins.js
+++ src/Components/Logins.js
@@ -1,23 +1,65 @@
 import React, { Component } from 'react';
+import { Button, FormGroup, FormControl} from "react-bootstrap";
+
 import '../logins.css';
 import Icons from './Icons';
 class Logins extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            Email: '',
+            Password: ''
+        }
+    }
+    handelChange = (e) =>{
+        this.setState({
+            [e.target.id] : e.target.value
+        });
+    }
+    handelSummit = (e) =>{
+         e.preventDefault();
+    }
+    handelEChange = (e) =>{
+        this.setState({Email:e.target.value})
+    }
+    handelPChange = (e) =>{
+        this.setState({Password:e.target.value})
+    }
+    validateForm(){
+        return this.state.Email.length > 0 && this.state.Password.length > 0;
+    }
+    printState(){
+        console.log(this.state.Password);
+        console.log(this.state.email);
+    }
     render() {
         return (
             <div className = "container">
-                <form>
-                <label className = "label-al">
-                Username:
-                <input type = "text" name = "name" />
-            
-                </label>
-                <label>
-                Password:
-                    <input type="text" name = "name" />
-                </label>
-                <button type="button" className="btn2 btn btn-secondary btn-lg">Large button</button>
-              
+                <form onSummit = {this.handelSummit}>
+                <FormGroup controlId="email" bsSize="large">
+            <label>Email</label>
+            <FormControl
+              autoFocus
+              type="email"
+              value={this.state.email}
+              onChange={this.handelEChange}
+            />
+          </FormGroup>
+          <FormGroup controlId="password" >
+            <label>Password</label>
+            <FormControl
+              value={this.state.password}
+              onChange={this.handelPChange}
+              type="password"
+            />
+          </FormGroup>
+                <Button block bsSize="large" disabled={!this.validateForm()} type="submit" className = "btn2 btn btn-secondary btn-lg">
+            Login
+          </Button>
+                <div className = "label2">
+                <label>Sign in with</label>
                 <Icons />
+                </div>
                 </form>
             
             </div>
