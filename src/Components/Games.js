092f0e165d6a15e7abc9d7520d96f17d4d765274
LandingHeart <land.liu0123@gmail.com>
2019-02-28 12:28:46 -0500

    updates files and changes styling

--- src/Components/Games.js
+++ src/Components/Games.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import '../CSS/body.css'
 
 class Games extends React.Component{
     render(){
         return(
         <div>
-        
-       game section
-       to be implemented, 
-       change html files to react js. 
+        <p className = "bd">
+            game section
+            to be implemented, 
+            change html files to react js. 
 
-
+          </p>
         </div>  
     )}
 }
