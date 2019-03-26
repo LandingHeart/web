dce4e4a2ebc195c5a3288b9e59b3ce3c7e9b94ed
LandingHeart <land.liu0123@gmail.com>
2019-03-26 12:44:57 -0400

    updates

--- client/src/Components/Footer.js
+++ client/src/Components/Footer.js
@@ -1,9 +1,17 @@
 import React from 'react'
-
+import '../CSS/footer.css'
 const Footer = () => {
     return(
-        <div className = "footer center">
-            app footer
+        <div className = "center footer">
+            <ul className="columns" data-columns="2">
+                <li>A</li>
+                <li>B</li>
+                <li>C</li>
+                <li>D</li>
+                <li>E</li>
+                <li>F</li>
+                <li>G</li>
+            </ul>
         </div>
     )
 }
