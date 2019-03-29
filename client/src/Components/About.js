c44a8cd0dfb67c0947899bf1cc7c9ded61046bc6
LandingHeart <land.liu0123@gmail.com>
2019-03-28 22:29:24 -0400

    edits

--- client/src/Components/About.js
+++ client/src/Components/About.js
@@ -12,12 +12,7 @@ const About = () =>{
             <div className = "content-container">
                
                 <div className="img-holder">
-                    <img src = {placeholder} alt = "placeholder" height = "200" width = "340"/>
-                    <div className = "imgholder2">
-                    <img src = {placeholder} alt = "placeholder" height = "200" width = "340"/>   
-                    </div> 
-                    <div className = "imgholder3">
-                    <img src = {placeholder} alt = "placeholder" height = "200" width = "340"/>   
+            
                     </div>                 
                     </div>
                 </div>
