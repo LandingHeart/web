839f947a258ae085d070894758256526228aaba4
LandingHeart <land.liu0123@gmail.com>
2019-03-29 22:54:14 -0400

    bug with express

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var app = express();
+var port = process.env.PORT || 8080;
 
+app.listen(port, function () {
+  console.log('Example app listening on port ' + port + '!');
+  
+});
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
