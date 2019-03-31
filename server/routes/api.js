9a5737e4c7736e2a8cc703d0b3c4505d2b311148
LandingHeart <land.liu0123@gmail.com>
2019-03-30 20:22:56 -0400

    adding api file

---
+++ server/routes/api.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+var app = express();
+var port = process.env.PORT || 8080;
+
+app.listen(port, function () {
+  console.log('Example app listening on port ' + port + '!');
+  
+});
+app.get('/api/getList', (req,res) => {
+  var list = ["item1", "item2", "item3"];
+  res.json(list);
+  console.log('Sent list of items');
+});
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
