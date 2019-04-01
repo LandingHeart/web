276312b078ace6a75eee9658af6de6f00c937711
LandingHeart <land.liu0123@gmail.com>
2019-04-01 18:37:25 -0400

    updates

--- api/app.js
+++ api/app.js
@@ -1,43 +1,35 @@
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-const React = require('react');
-
+const normalizePort = require('normalize-port');
+const server = express();
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var testAPIRouter = require("./routes/testAPI");
+var cors = require("cors");
+
 
 var app = express();
-app.use(express.static(path.join(__dirname, './public')));
-
 // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.set('views', __dirname + '/views');
-app.engine('html', engines.mustache);
-app.set('view engine', 'html');
+app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use('/', indexRouter);
-app.use('/',(req,res) =>{
-   res.render('../client/public/index.html');
-});
+app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.use("/testAPI", testAPIRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
-// app.get('/', function(req, res) {
-//   var markup = React.renderToString(<APP />);  // <-- render the APP here
-//   res.send(markup);                          // <-- response with the component
-// });
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
@@ -49,6 +41,5 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.listen(3000,()=>{console.log('running')});
 
 module.exports = app;
